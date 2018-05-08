61c4c2f10f329c8c2e728e646216f0a2607296ee
Ryan Malencia <ryan.malencia@pitt.edu>
2018-05-07 20:10:24 -0400

    Signalr Hub Added

---
+++ HomeControlServer/SignalR/PlugStatus.cs
@@ -0,0 +1,27 @@
+﻿using HomeControlServer.Models;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+
+namespace HomeControlServer.SignalR
+{
+    public class PlugStatus
+    {
+        private readonly static Lazy<PlugStatus> _instance = new Lazy<PlugStatus>(() => new PlugStatus(GlobalHost.ConnectionManager.GetHubContext<PlugStatusHub>().Clients));
+
+        private IHubConnectionContext<dynamic> Clients { get; set; }
+
+        private PlugStatus(IHubConnectionContext<dynamic> clients)
+        {
+            Clients = clients;
+        }
+
+        public static PlugStatus Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
---
+++ HomeControlServer/SignalR/PlugStatusHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace HomeControlServer.SignalR
+{
+    [HubName("PlugStatusHub")]
+    public class PlugStatusHub : Hub
+    {
+        private readonly PlugStatus _plugStatus;
+
+        public PlugStatusHub() : this(PlugStatus.Instance)
+        {
+
+        }
+
+        public PlugStatusHub(PlugStatus plugStatus)
+        {
+            _plugStatus = plugStatus;
+        }
+    }
+} 
